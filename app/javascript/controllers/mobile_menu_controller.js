fb5fc26bb1d0a2f6a73802b8fbfadce833e33e2e
Ahmed Hannan <ahmedhannansidra2@gmail.com>
2025-12-26 21:26:45 +0500

    Modify navbar

---
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu"]
+
+  connect() {
+    // menu starts hidden
+    this.menuTarget.classList.add("-translate-x-full")
+  }
+
+  toggle() {
+    this.menuTarget.classList.toggle("-translate-x-full")
+    this.menuTarget.classList.toggle("translate-x-0")
+  }
+}
