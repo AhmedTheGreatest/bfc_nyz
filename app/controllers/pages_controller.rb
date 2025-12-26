4353a9c9e9a967ede2ba9da9ae1ebb9394e10313
Ahmed Hannan <ahmedhannansidra2@gmail.com>
2025-12-26 20:50:32 +0500

    Add Pages controller and setup basic layout

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def academics
+  end
+
+  def facilities
+  end
+
+  def contact
+  end
+end
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import daisyui from "daisyui"
+
+export default {
+  content: [
+    "./app/views/**/*.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js"
+  ],
+  plugins: [daisyui],
+}
