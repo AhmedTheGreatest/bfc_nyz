f125564304e060fedb8d39161621fee69293ebb1
Ahmed Hannan <ahmedhannansidra2@gmail.com>
2025-12-28 17:03:27 +0500

    Add image display on Announcements#index view

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,2 +1,25 @@
 module AnnouncementsHelper
+  def announcement_excerpt(rich_text, length: 160)
+    html = rich_text.to_s
+
+    # Remove single attachments
+    html = html.gsub(
+      /<action-text-attachment[\s\S]*?<\/action-text-attachment>/,
+      ""
+    )
+
+    # Remove attachment galleries
+    html = html.gsub(
+      /<div class="attachment-gallery"[\s\S]*?<\/div>/,
+      ""
+    )
+
+    # Remove empty paragraphs left behind
+    html = html.gsub(/<p>\s*<\/p>/, "")
+
+    # Strip remaining HTML
+    text = strip_tags(html).squish
+
+    truncate(text, length: length)
+  end
 end
