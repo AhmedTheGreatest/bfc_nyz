af9722015f4a6bef901669db836261b599c30434
Ahmed Hannan <ahmedhannansidra2@gmail.com>
2026-01-21 20:20:17 +0500

    Fix a bug in which the navbar was stuck on mobile and also hook up admission buttons

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -4,12 +4,43 @@ export default class extends Controller {
   static targets = ["menu"]
 
   connect() {
-    // menu starts hidden
-    this.menuTarget.classList.add("-translate-x-full")
+    // Start hidden
+    this.hideMenu(false)
+
+    // Fix for back/forward navigation in browser
+    window.addEventListener("pageshow", () => {
+      this.hideMenu(false)
+    })
   }
 
   toggle() {
-    this.menuTarget.classList.toggle("-translate-x-full")
-    this.menuTarget.classList.toggle("translate-x-0")
+    if (this.menuTarget.classList.contains("-translate-x-full")) {
+      this.showMenu()
+    } else {
+      this.hideMenu()
+    }
+  }
+
+  showMenu() {
+    this.menuTarget.classList.remove("-translate-x-full")
+    this.menuTarget.classList.add("translate-x-0")
+    document.body.classList.add("overflow-hidden") // prevent scroll behind menu
+  }
+
+  hideMenu(animate = true) {
+    if (animate) {
+      this.menuTarget.classList.add("-translate-x-full")
+      this.menuTarget.classList.remove("translate-x-0")
+    } else {
+      // immediately hide without animation (used on page load/back)
+      this.menuTarget.classList.add("-translate-x-full")
+      this.menuTarget.classList.remove("translate-x-0")
+      this.menuTarget.classList.remove("transition-transform", "duration-300")
+      setTimeout(() => {
+        // restore classes for next toggle
+        this.menuTarget.classList.add("transition-transform", "duration-300")
+      }, 10)
+    }
+    document.body.classList.remove("overflow-hidden")
   }
 }
